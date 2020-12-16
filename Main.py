b432598d87708708e4155b234b99668d1d891635
Florian Hammer <florian.hammer.1996@web.de>
2020-12-16 14:42:56 +0100

    get all participants of a chat

--- Main.py
+++ Main.py
@@ -3,37 +3,26 @@ from telethon.sync import TelegramClient
 from telethon.tl.functions.messages import GetDialogsRequest
 from telethon.tl.types import InputPeerEmpty
 
-import Credentials as crd
+import Constants as crd
 
-client = TelegramClient('anon',crd.api_id, crd.api_hash)
+client = TelegramClient(
+            'anon', 
+            crd.credentials['api_id'], 
+            crd.credentials['api_hash']
+        )
+
+
 async def main():
     # log into telegram
     await client.start()
 
-    chats = []
-    last_date = None
-    chunk_size = 200
-    groups=[]
-    result = await client(
-                GetDialogsRequest(
-                    offset_date=last_date,
-                    offset_id=0,
-                    offset_peer=InputPeerEmpty(),
-                    limit=chunk_size,
-                    hash = 0
-                )
-            )
-    chats.extend(result.chats)
-    
-    for chat in chats:
-        try:
-            if chat.megagroup== True:
-                groups.append(chat)
-                print("Megagroup")
-                print(chat.stringify())
-        except:
-            continue
-    print(groups)
+    chat = await client.get_entity('https://t.me/a27362672727486747373728')
+    # chat = await client.get_entity('https://t.me/geekschannel')
+    users = await client.get_participants(chat)
+    for user in users:
+        if user.username is not None:
+            print(user.username)
+
 
 with client:
     client.loop.run_until_complete(main())
