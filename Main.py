1b2e8fdf808a35644777e2b671e63fa22962cd0f
Florian Hammer <florian.hammer.1996@web.de>
2020-12-19 11:04:04 +0100

    added to current dev level for collab

--- Main.py
+++ Main.py
@@ -14,15 +14,29 @@ client = TelegramClient(
 
 async def main():
     # log into telegram
-    await client.start()
+    await client.start() # creates .session file for future usage
 
-    chat = await client.get_entity('https://t.me/a27362672727486747373728')
-    # chat = await client.get_entity('https://t.me/geekschannel')
+    #chat = await client.get_entity('https://t.me/a27362672727486747373728')
+    chat = await client.get_entity('https://t.me/joinchat/VfhhtxOgLKILbKrKB4Y3TA')
+    #chat = await client.get_entity('https://t.me/geekschannel')
+    #chat = await client.get_entity('https://t.me/qanon_world')
+
+    print(chat.stringify())
+    #await get_all_messages(chat)
+    #await get_all_participants(chat)
+
+async def get_all_messages(chat):
+    messages = await client.get_messages(chat, None, reverse=True)
+    for message in messages:
+        print(message.stringify())
+
+
+async def get_all_participants(chat):
     users = await client.get_participants(chat)
     for user in users:
-        if user.username is not None:
-            print(user.username)
-
+        print(user.stringify())
+        #if user.username is not None:
+        #    print(user.username)
 
 with client:
     client.loop.run_until_complete(main())
