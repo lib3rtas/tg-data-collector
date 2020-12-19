c5286e63dd8a8bbe71d789f92c4b91f33df441d0
Florian Hammer <florian.hammer.1996@web.de>
2020-12-19 11:17:08 +0100

    added comment

--- Constants.py
+++ Constants.py
@@ -1,4 +1,5 @@
+# To get an api_id and api_hash go to https://my.telegram.org and create a new app
 credentials = {
-    "api_id" : 1234,
-    "api_hash" : "Hash12345678AEFGH"
+    "api_id": 1234,
+    "api_hash": "Hash12345678AEFGH"
 }
