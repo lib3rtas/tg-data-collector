84d4c1c40b7b3c304dfe49dc06a95b560eb3716d
Fyodor Perejoguine <fyodordev@gmail.com>
2020-12-20 09:48:48 +0100

    Draw graph, load from file.
    Make downloading messages optional.
    Add group title as edge attribute.

--- Main.py
+++ Main.py
@@ -1,11 +1,19 @@
 from telethon.sync import TelegramClient
 from telethon.tl.functions.messages import GetDialogsRequest
 from telethon.tl.types import InputPeerEmpty
+import logging
+import networkx as nx
+import os
+import networkx as nx
+from networkx.algorithms import bipartite
+import matplotlib.pyplot as plt
 
 from typing import List
 
 import Constants as const
 from process_group import get_groups_data
+logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s',
+                    level=logging.INFO)
 
 client = TelegramClient(
             'anon', 
@@ -15,8 +23,43 @@ client = TelegramClient(
 
 
 async def main():
-    hints = ["https://t.me/joinchat/VfhhtxOgLKILbKrKB4Y3TA"]
-    await get_groups_data(client, hints)
+    hints = [
+        "@donald_j_trump_q_family_germany",
+        "@defender_shaef_2q2q",
+        "@qanons_deutschland",
+        "@qanon_world",
+        "@kaisertreu_ewig",
+        "@deutschlandtreff",
+        "@digitalerchronist_allgemeinchat",
+        "@betfury_de",
+        "@cryptomondayde",
+        "@gaynudesger",
+        "@pokemongoger",
+        "@germanfurrygroup",
+        "@binancegerman",
+        "@technicall_crypto",
+    ]
+    print('downloading from telegram API')
+    groups_users_graph, all_messages_df = await get_groups_data(client, hints)
+    #print('loading from file')
+    #groups_users_graph = nx.read_gexf(os.path.join('data', 'groups_users_graph.gexf'))
+
+    user_count = groups_users_graph.number_of_nodes()
+    print('user nodes: ' + str(user_count))
+    print('transforming network')
+    group_nodes = [
+        x for x, y
+        in groups_users_graph.nodes(data=True)
+        if y['type'] == 'group'
+    ]
+    groups_graph = bipartite.weighted_projected_graph(
+        groups_users_graph, group_nodes)
+
+    print('drawing plot')
+    plt.subplot(121)
+    nx.draw(groups_graph, with_labels=True, font_weight='bold')
+    plt.show()
+
 
 with client:
     client.loop.run_until_complete(main())
--- process_group.py
+++ process_group.py
@@ -10,7 +10,11 @@ from telethon.sync import TelegramClient
 from utils import save_df, load_df, serialize_dict
 
 
-async def process_group(link_hint: str, client: TelegramClient):
+async def process_group(
+    link_hint: str,
+    client: TelegramClient,
+    process_messages=False,
+):
     await client.start()  # creates .session file for future usage
     group = await client.get_entity(link_hint)
 
@@ -20,52 +24,49 @@ async def process_group(link_hint: str, client: TelegramClient):
         'participants_count': group .participants_count,
         'date_created': group .date,
     }
+    print(f'Processing group {group_info["title"]}')
 
-    date_to = datetime(2020, 12, 10)
-
-    messages_dict = []
-    start_time = time.time()
-
-    count = 0
-
-    while not messages_dict or (
-        messages_dict[-1]['timestamp'].replace(tzinfo=None)
-        > date_to.replace(tzinfo=None)
-    ):
-        count +=1
-        offset = (
-            messages_dict[-1]["timestamp"]
-            if messages_dict else None
-        )
-        print(f'request nr. {count}, offset is {offset}')
-        messages = await client.get_messages(
-            group,
-            limit=100,
-            offset_date=offset,
-            reverse=False,
-        )
-        for m in messages:
-            if m.from_id is not None:
-                fwd_from_id = None
-                if m.fwd_from is not None:
-                    if hasattr(m.fwd_from.from_id, 'user_id'):
-                        fwd_from_id = m.fwd_from.from_id.user_id
-                    elif hasattr(m.fwd_from.from_id, 'channel_id'):
-                        fwd_from_id = m.fwd_from.from_id.channel_id
-                messages_dict.append({
-                    'message_id': m.id,
-                    'user_id': m.from_id.user_id,
-                    'group_id': group.id,
-                    'timestamp': m.date,
-                    'message_text': m.message,
-                    'fwd_from_chat_id': fwd_from_id,
-                })
-
-    print(f'Fetched {len(messages_dict)} messages in'
-            f'{time.time() - start_time} seconds.')
-
-
-    messages_df = pd.DataFrame(messages_dict)
+    messages_df = None
+    if process_messages:
+        date_to = datetime(2020, 12, 10)
+        messages_dict = []
+        start_time = time.time()
+        count = 0
+        while not messages_dict or (
+            messages_dict[-1]['timestamp'].replace(tzinfo=None)
+            > date_to.replace(tzinfo=None)
+        ):
+            count +=1
+            offset = (
+                messages_dict[-1]["timestamp"]
+                if messages_dict else None
+            )
+            print(f'request nr. {count}, offset is {offset}')
+            messages = await client.get_messages(
+                group,
+                limit=100,
+                offset_date=offset,
+                reverse=False,
+            )
+            for m in messages:
+                if m.from_id is not None:
+                    fwd_from_id = None
+                    if m.fwd_from is not None:
+                        if hasattr(m.fwd_from.from_id, 'user_id'):
+                            fwd_from_id = m.fwd_from.from_id.user_id
+                        elif hasattr(m.fwd_from.from_id, 'channel_id'):
+                            fwd_from_id = m.fwd_from.from_id.channel_id
+                    messages_dict.append({
+                        'message_id': m.id,
+                        'user_id': m.from_id.user_id,
+                        'group_id': group.id,
+                        'timestamp': m.date,
+                        'message_text': m.message,
+                        'fwd_from_chat_id': fwd_from_id,
+                    })
+        print(f'Fetched {len(messages_dict)} messages in'
+                f'{time.time() - start_time} seconds.')
+        messages_df = pd.DataFrame(messages_dict)
 
     users = await client.get_participants(group)
     users_list = [{
@@ -84,16 +85,18 @@ async def process_group(link_hint: str, client: TelegramClient):
 async def get_groups_data(client: TelegramClient, group_hints: List[str]):
     all_messages_df = None
     groups_users_graph = nx.Graph()
+    user_count = 0
 
     for group in group_hints:
         group_info, users_list, messages_df = await process_group(
             group,
             client
         )
-        if all_messages_df is None:
-            all_messages_df = messages_df
-        else:
-            all_messages_df = pd.concat((all_messages_df, messages_df), axis=0)
+        if messages_df is not None:
+            if all_messages_df is None:
+                all_messages_df = messages_df
+            else:
+                all_messages_df = pd.concat((all_messages_df, messages_df), axis=0)
 
         groups_users_graph.add_node('group_' + str(group_info['group_id']),
                                     **{
@@ -101,6 +104,7 @@ async def get_groups_data(client: TelegramClient, group_hints: List[str]):
                                         'subtype': 'chat',
                                         **serialize_dict(group_info),
                                     })
+        user_count += len(users_list)
 
         groups_users_graph.add_nodes_from([('user_' + str(u['user_id']),
                                             {
@@ -111,9 +115,11 @@ async def get_groups_data(client: TelegramClient, group_hints: List[str]):
         groups_users_graph.add_edges_from([(
             'user_' + str(u['user_id']),
             'group_' + str(group_info['group_id'])
-        ) for u in users_list])
+        ) for u in users_list], group_title=group_info['title'])
 
-    save_df(all_messages_df, 'group_messages')
-    nx.write_gexf(groups_users_graph, os.path.join('data', 'groups_users_graph'))
+    #save_df(all_messages_df, 'group_messages')
+    print('total users: ' + str(user_count))
+    print('writing to file')
+    nx.write_gexf(groups_users_graph, os.path.join('data', 'groups_users_graph.gexf'))
 
     return groups_users_graph, all_messages_df
